f907e6a0092eb50d2c56f473078f1603d0981c52
Giovanni Arenare Mota <giovanniarenaremota@gmail.com>
2026-09-10 11:29:36 -0300

    Add files via upload

---
+++ Login/src/main/java/com/example/Login/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.example.Login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller 
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+    
+    @PostMapping("/register")
+    public String handleRegister(
+            @RequestParam("nome") String nome,
+            @RequestParam("email") String email,
+            @RequestParam("cpf") String cpf,
+            @RequestParam("rg") String rg,
+            @RequestParam("endereco") String endereco,
+            @RequestParam("instituicao") String instituicao,
+            @RequestParam("senha") String senha) {
+
+        System.out.println("Registro: Redirecionado para a página de login.");
+        return "redirect:/login"; 
+    }
+
+    @GetMapping("/recoverpassword")
+    public String recoverpassword() {
+        return "recoverpassword";
+    }
+
+    @PostMapping("/recoverpassword")
+    public String handleRecoverPassword(
+            @RequestParam("email") String email) {
+        System.out.println("Recuperação de E-mail: Redirecionado para a página de login.");
+        return "redirect:/login"; 
+    }
+}
